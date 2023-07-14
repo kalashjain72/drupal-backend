f2b0f563ec715afa43439b64bde2602059a28d44
kalashjain72 <kalashjain72@gmail.com>
2023-07-14 09:41:58 +0530

    FT2023-378: custom module creted

--- web/modules/custom/hello_user/src/Controller/HelloUserController.php
+++ web/modules/custom/hello_user/src/Controller/HelloUserController.php
@@ -4,7 +4,6 @@ namespace Drupal\hello_user\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-
 /**
  * Returns responses for hello user routes.
  */
@@ -14,15 +13,17 @@ final class HelloUserController extends ControllerBase {
    * Builds the response.
    */
   public function __invoke(): array {
+    // Get the current user object.
     $user = \Drupal::currentUser();
+    // Get the display name of the user.
     $name = $user->getDisplayName();
+    // Prepare the render array for the content.
     $build['content'] = [
       '#type' => 'item',
       '#markup' => $this->t('Hello @name', ['@name' => $name]),
     ];
-
+    // Return the render array.
     return $build;
   }
 
 }
-?>
