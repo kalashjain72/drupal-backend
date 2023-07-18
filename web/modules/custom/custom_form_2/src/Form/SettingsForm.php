9547c39cf94986232bcba4612edba324f5a83d9c
kalashjain72 <kalashjain72@gmail.com>
2023-07-18 11:57:50 +0530

    FT2023-413: task 2 validation using ajax complited

--- web/modules/custom/custom_form_2/src/Form/SettingsForm.php
+++ web/modules/custom/custom_form_2/src/Form/SettingsForm.php
@@ -4,7 +4,8 @@ namespace Drupal\custom_form_2\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\HtmlCommand;
 
 
 /**
@@ -32,34 +33,24 @@ class SettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('custom_form_2.settings');
 
+    $form['element'] = [
+			'#type' => 'markup',
+			'#markup' => "<div class='success'></div>",
+		];
+    
     $form['full_name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Full Name'),
       '#default_value' => $config->get('full_name'),
       '#required' => TRUE,
     ];
-
 		
     $form['phone_number'] = [
       '#type' => 'tel',
       '#title' => $this->t('Phone Number'),
       '#default_value' => $config->get('phone_number'),
       '#required' => TRUE,
-      '#ajax' => [
-        'callback' => '::validatePhoneNumberAjax',
-        'event' => 'change',
-        'wrapper' => 'phone-number-validation-message',
-        'progress' => [
-          'type' => 'throbber',
-          'message' => NULL,
-        ],
-      ],
-    ];
-
-    $form['phone_number_validation_message'] = [
-      '#type' => 'markup',
-      '#prefix' => '<div id="phone-number-validation-message">',
-      '#suffix' => '</div>',
+      '#suffix' => '<div class = "error" id = "phone_number"></div>'
     ];
 
     $form['email'] = [
@@ -67,23 +58,8 @@ class SettingsForm extends ConfigFormBase {
       '#title' => $this->t('Email ID'),
       '#default_value' => $config->get('email'),
       '#required' => TRUE,
-      '#ajax' => [
-        'callback' => '::validateEmailAjax',
-        'event' => 'change',
-        'wrapper' => 'email-validation-message',
-        'progress' => [
-          'type' => 'throbber',
-          'message' => NULL,
-        ],
-      ],
+      '#suffix' => '<div class = "error" id = "email"></div>'
     ];
-
-    $form['email_validation_message'] = [
-      '#type' => 'markup',
-      '#prefix' => '<div id="email-validation-message">',
-      '#suffix' => '</div>',
-    ];
-
 
 		$form['gender'] = [
 			'#type' => 'radios',
@@ -94,63 +70,59 @@ class SettingsForm extends ConfigFormBase {
         'other' => $this->t('Other'),
       ]
 		];
-		return parent::buildForm($form, $form_state);
+    
+		
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t("submit"),
+			'#ajax' => [
+						'callback' => '::validateAjax',
+			],
+		];
+
+		$form['#attatched']['library'][] = 'custom_form_2/custom_form_2_css';
+		return $form;
+	
 	}
 
 	/**
    * {@inheritdoc}
    */
 
+  /**
+   * Ajax callback to validate the phone number and email field.
+   */
   
-  /**
-   * Ajax callback to validate the phone number field.
-   */
-  public function validatePhoneNumberAjax(array &$form, FormStateInterface $form_state) {
-    $phone_number = $form_state->getValue('phone_number');
+  public function validateAjax(array &$form, FormStateInterface $form_state) {
+		$ajax_response = new AjaxResponse();
 
-    // Validate phone number.
-    if (!is_numeric($phone_number) || strlen($phone_number) != 10) {
-      $error_message = $this->t('A phone number needs to have exactly 10 digits.');
-      $form_state->setErrorByName('phone_number', $error_message);
-
-      return new JsonResponse(['error' => $error_message]);
-    }
-
-    return new JsonResponse(['success' => TRUE]);
-  }
-
-  /**
-   * Ajax callback to validate the email field.
-   */
-  public function validateEmailAjax(array &$form, FormStateInterface $form_state) {
     $email = $form_state->getValue('email');
+    $phoneNumber = $form_state->getValue('phone_number');
 
-    // Validate email format.
+		$valid = TRUE;
+    // Validate phone number.
+    if (!is_numeric($phoneNumber) || strlen($phoneNumber) != 10) {
+      $ajax_response->addCommand(new HtmlCommand('#phone_number', 
+        $this->t('A phone number needs to have exactly 10 digits.')));
+      $valid = FALSE;
+    }
+
+    // Validate email.
     if (!\Drupal::service('email.validator')->isValid($email)) {
-      $error_message = $this->t('Invalid email format.');
-      $form_state->setErrorByName('email', $error_message);
-
-      return new JsonResponse(['error' => $error_message]);
+			$ajax_response->addCommand(new HtmlCommand('#email', $this->t('Invalid format')));
     }
-
-    // Separate domain validation (Yahoo, Gmail, or Outlook) from email format validation.
-    if (!preg_match('/@(yahoo|gmail|outlook)\.com$/', $email)) {
-      $error_message = $this->t('Use email from Yahoo, Gmail, or Outlook.');
-      $form_state->setErrorByName('email', $error_message);
-
-      return new JsonResponse(['error' => $error_message]);
+    elseif (!preg_match('/@(yahoo|gmail|outlook)\.com$/', $email)) {
+      $ajax_response->addCommand(new HtmlCommand('#email', $this->t('Use email from Yahoo, Gmail, or Outlook.')));
     }
-
-    return new JsonResponse(['success' => TRUE]);
+		if ($valid) {
+		$ajax_response->addCommand(new HtmlCommand('.success', 'Form submitted successfully'));
+		}
+		return $ajax_response;
   }
-
-
 
 	/**
    * {@inheritdoc}
    */
 	public function submitForm(array &$form, FormStateInterface $form_state) {
-    $submitted_name = $form_state->getValue('full_name');
-    $this->messenger()->addMessage($this->t("@user Your Form has been submitted Successfully", ['@user' => $submitted_name]));
-  }
+    }
 }
